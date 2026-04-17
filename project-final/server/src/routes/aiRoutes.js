f5903fc7cc2cfa61830c1688ac5f23b9c61ade23
DEV KUMAR SHARMA <166902599+0001expect@users.noreply.github.com>
2026-04-17 14:48:43 +0530

    Delete project-final directory

--- project-final/server/src/routes/aiRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { generateSummary } = require('../controllers/aiController');
-const { protect } = require('../middlewares/authMiddleware');
-
-router.post('/generate-summary', protect, generateSummary);
-
-module.exports = router;
